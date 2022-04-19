1ac0895588b639661a1bc7b6a6483e3d475dcf44
administrador <lucasserafin2000@gmail.com>
2022-04-19 20:19:28 -0300

    Versión inicial

---
+++ nombre_solucion/nombre-proyecto-mvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using nombre_proyecto_mvc.Models;
+
+namespace nombre_proyecto_mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ nombre_solucion/nombre-proyecto-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace nombre_proyecto_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
